2927b7d4bfc102e7e71cfdc5c56b2690d36f5bbc
KimSungGyun <egoksg@naver.com>
2016-07-01 00:57:04 +0900

    ..

--- YeaMin/src/main/java/com/yeamin/controller/UserController.java
+++ YeaMin/src/main/java/com/yeamin/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.yeamin.controller;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,11 +9,18 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class UserController {
 
+	@Autowired
+	private SqlSession sqlSession;
+	
 	@RequestMapping("/main.do")
 	public ModelAndView aaa() {
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("contentViewName", "/WEB-INF/jsp/main.jsp");
 		mav.setViewName("/WEB-INF/jsp/template/template.jsp");
+		
+		int count = sqlSession.selectOne("user.QID_SELECT_USER_COUNT");
+		System.out.println(count);
+		
 		return mav;
 	}
 	
