fc766f4925dd7c4e486d6e6ea6b0e579e03e5e68
KimSungGyun <egoksg@naver.com>
2016-07-05 01:16:16 +0900

    Constants 추가

---
+++ YeaMin/src/main/java/com/yeamin/constants/AppConstants.java
@@ -0,0 +1,11 @@
+package com.yeamin.constants;
+
+public class AppConstants {
+
+	public final static String TEMPLATE_VIEW_PATH = "/WEB-INF/jsp/template/template.jsp";
+	
+	private AppConstants() {
+		
+	}
+	
+}
---
+++ YeaMin/src/main/java/com/yeamin/constants/QueryConstants.java
@@ -0,0 +1,11 @@
+package com.yeamin.constants;
+
+public class QueryConstants {
+	
+	public final static String QID_SELECT_USER_COUNT = "user.QID_SELECT_USER_COUNT";
+	
+	private QueryConstants() {
+		
+	}
+	
+}
