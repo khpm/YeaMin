50d3ec4f6ee96579000499fb7e7820540e0fc454
KimSungGyun <egoksg@naver.com>
2016-07-08 13:51:32 +0900

    YM_STORE_IMG 기본키 추가

--- YeaMin/src/main/java/com/yeamin/dto/StoreImgDto.java
+++ YeaMin/src/main/java/com/yeamin/dto/StoreImgDto.java
@@ -2,28 +2,28 @@ package com.yeamin.dto;
 
 public class StoreImgDto {
 
-	private String store_img_origin_path;
-
 	private String store_img_system_path;
+	
+	private String store_img_origin_path;
 
 	public StoreImgDto() {
 		
 	}
-
-	public String getStore_img_origin_path() {
-		return store_img_origin_path;
-	}
-
-	public void setStore_img_origin_path(String store_img_origin_path) {
-		this.store_img_origin_path = store_img_origin_path;
-	}
-
+	
 	public String getStore_img_system_path() {
 		return store_img_system_path;
 	}
 
 	public void setStore_img_system_path(String store_img_system_path) {
 		this.store_img_system_path = store_img_system_path;
+	}
+	
+	public String getStore_img_origin_path() {
+		return store_img_origin_path;
+	}
+
+	public void setStore_img_origin_path(String store_img_origin_path) {
+		this.store_img_origin_path = store_img_origin_path;
 	}
 
 }
