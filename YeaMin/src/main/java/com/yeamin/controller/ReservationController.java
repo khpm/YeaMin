3cad46ec7c9af263c5fa6aea3dd51d0ea1d6bcf6
KimSungGyun <egoksg@naver.com>
2016-07-13 03:21:34 +0900

    예약 시간 관리 화면 추가

--- YeaMin/src/main/java/com/yeamin/controller/ReservationController.java
+++ YeaMin/src/main/java/com/yeamin/controller/ReservationController.java
@@ -4,7 +4,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.yeamin.constants.AppConstants;
 import com.yeamin.dao.ReservationDao;
 import com.yeamin.dao.StatsDao;
 
@@ -21,6 +24,13 @@ public class ReservationController {
 	
 	public ReservationController() {
 		
+	}
+	
+	@RequestMapping("/reservationCapacityList.do")
+	public ModelAndView reservationCapacityList() {
+		ModelAndView mav = new ModelAndView(AppConstants.TEMPLATE_VIEW_PATH);
+		mav.addObject("contentViewName", "/WEB-INF/jsp/reservation/reservationCapacityList.jsp");
+		return mav;
 	}
 	
 }
