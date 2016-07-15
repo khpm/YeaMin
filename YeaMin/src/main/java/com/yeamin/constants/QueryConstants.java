6589be45f5d24d5c3dc3b6a289750c2687e84068
JHY <JHY@JHY-PC>
2016-07-15 12:41:01 +0900

    QueryConstants 상품 카테고리 + 예약시간 병합

--- YeaMin/src/main/java/com/yeamin/constants/QueryConstants.java
+++ YeaMin/src/main/java/com/yeamin/constants/QueryConstants.java
@@ -8,12 +8,11 @@ public class QueryConstants {
 	public final static String QID_UPDATE_RESERVATION_CAPACITY = "reservation.QID_UPDATE_RESERVATION_CAPACITY";
 	public final static String QID_DELETE_RESERVATION_CAPACITY = "reservation.QID_DELETE_RESERVATION_CAPACITY";
 	
-	public final static String QID_SELECT_PRODUCT_CATEGORY_LIST = "product.QID_SELECT_PRODUCT_CATEGORY_LIST";
-	
+	public final static String QID_SELECT_PRODUCT_CATEGORY_LIST = "product.QID_SELECT_PRODUCT_CATEGORY_LIST";	
 	public final static String QID_SELECT_PRODUCT_CATEGORY_LIST_ONE = "product.QID_SELECT_PRODUCT_CATEGORY_LIST_ONE";
-	
 	public final static String QID_SELECT_PRODUCT_CATEGORY_LIST_ONE2 = "product.QID_SELECT_PRODUCT_CATEGORY_LIST_ONE2";
-	
+	public final static String QID_DELETE_PRODUCT_CATEGORY = "product.QID_DELETE_PRODUCT_CATEGORY";
+
 	private QueryConstants() {
 		
 	}
