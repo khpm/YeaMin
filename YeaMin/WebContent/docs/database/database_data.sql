818425efcf0eb72433cde213b5577d7bdfe59054
KimSungGyun <egoksg@naver.com>
2016-07-29 00:43:18 +0900

    테스트 일반 사용자 - 여성/남성 사용자 추가

--- YeaMin/WebContent/docs/database/database_data.sql
+++ YeaMin/WebContent/docs/database/database_data.sql
@@ -1,2 +1,3 @@
-INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'admin', 'admin', '사장님', 'egoksg@naver.com', '01050693104', sysdate, 'F', 'Y');
-
+INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'admin', 'admin', '사장님', 'egoksg@naver.com', '010-5069-3104', sysdate, 'M', 'Y');
+INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'male_user', '123', '남성 사용자', 'male@naver.com', '010-5069-3104', sysdate, 'M', 'N');
+INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'female_user', '123', '여성 사용자', 'female@naver.com', '010-5069-3104', sysdate, 'F', 'N');
