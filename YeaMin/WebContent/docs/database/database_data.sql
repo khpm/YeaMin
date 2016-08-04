57a74d03820e531718e02d4fcba7bce124418c7b
KimSungGyun <egoksg@naver.com>
2016-08-04 20:11:32 +0900

    데이터 다아아아아

--- YeaMin/WebContent/docs/database/database_data.sql
+++ YeaMin/WebContent/docs/database/database_data.sql
@@ -1,3 +1,13 @@
 INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'admin', 'admin', '사장님', 'egoksg@naver.com', '010-5069-3104', sysdate, 'M', 'Y');
-INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'male_user', '123', '남성 사용자', 'male@naver.com', '010-5069-3104', sysdate, 'M', 'N');
-INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'female_user', '123', '여성 사용자', 'female@naver.com', '010-5069-3104', sysdate, 'F', 'N');
+INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'user_m_20', '123', '20대 남성 사용자', 'male20@naver.com', '010-5069-3104', TO_DATE('1990/11/04', 'YYYY/MM/DD'), 'M', 'N');
+INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'user_m_30', '123', '30대 남성 사용자', 'male30@naver.com', '010-5069-3104', TO_DATE('1980/11/04', 'YYYY/MM/DD'), 'M', 'N');
+INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'user_m_40', '123', '40대 남성 사용자', 'male40@naver.com', '010-5069-3104', TO_DATE('1970/11/04', 'YYYY/MM/DD'), 'M', 'N');
+INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'user_m_50', '123', '50대 남성 사용자', 'male50@naver.com', '010-5069-3104', TO_DATE('1960/11/04', 'YYYY/MM/DD'), 'M', 'N');
+INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'user_m_60', '123', '60대 남성 사용자', 'male60@naver.com', '010-5069-3104', TO_DATE('1950/11/04', 'YYYY/MM/DD'), 'M', 'N');
+INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'user_f_20', '123', '20대 여성 사용자', 'female20@naver.com', '010-5069-3104', TO_DATE('1990/11/04', 'YYYY/MM/DD'), 'F', 'N');
+INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'user_f_30', '123', '30대 여성 사용자', 'female30@naver.com', '010-5069-3104', TO_DATE('1980/11/04', 'YYYY/MM/DD'), 'F', 'N');
+INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'user_f_40', '123', '40대 여성 사용자', 'female40@naver.com', '010-5069-3104', TO_DATE('1970/11/04', 'YYYY/MM/DD'), 'F', 'N');
+INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'user_f_50', '123', '50대 여성 사용자', 'female50@naver.com', '010-5069-3104', TO_DATE('1960/11/04', 'YYYY/MM/DD'), 'F', 'N');
+INSERT INTO ym_user VALUES(ym_user_no_seq.nextval, 'user_f_60', '123', '60대 여성 사용자', 'female60@naver.com', '010-5069-3104', TO_DATE('1950/11/04', 'YYYY/MM/DD'), 'F', 'N');
+
+commit;
